dd0d61f70a827222bba639e098f1e966630f1e10
Lars van Herk <dev@vanherk.io>
2022-08-06 18:44:26 +0200

    Add traceroute logging

--- index.js
+++ index.js
@@ -2,46 +2,94 @@ import { fileURLToPath } from 'url';
 import { resolve as resolvePath, dirname } from 'path';
 import { writeFileSync } from 'fs';
 import { resolve, setServers } from 'dns';
+import { spawn } from 'child_process';
 
 import mkdirp from 'mkdirp';
+
+await mkdirp(resolvePath(dirname(fileURLToPath(import.meta.url)), 'output'));
 
 const results = {
   dnsLookup: {
     openDNS: [],
     cloudflare: [],
     google: []
+  },
+  traceroute: {
+    'vanherk.io': [],
+    'google.com': []
   }
 };
 
-await mkdirp(resolvePath(dirname(fileURLToPath(import.meta.url)), 'output'));
-
-setInterval(async () => {
-  const resolveForNS = (provider, servers) => {
-    return new Promise((resolvePromise) => {
-      setServers(servers);
-      resolve('google.com', 'A', (err, records) => {
-        if (err) {
-          results.dnsLookup[provider].push({
-            timestamp: new Date().toISOString(),
-            error: err
-          });
-        }
-  
+const resolveForNS = (provider, servers) => {
+  return new Promise((resolvePromise) => {
+    setServers(servers);
+    resolve('vanherk.io', 'A', (err, records) => {
+      if (err) {
         results.dnsLookup[provider].push({
           timestamp: new Date().toISOString(),
-          result: records
+          error: err
         });
+      }
 
-        resolvePromise();
+      results.dnsLookup[provider].push({
+        timestamp: new Date().toISOString(),
+        result: records
       });
-    });
-  };
 
+      resolvePromise();
+    });
+  });
+};
+
+const preformTraceroute = (domain) => {
+  return new Promise(async (resolvePromise) => {
+    const trace = {
+      timestamp: new Date().toISOString(),
+      data: ''
+    };
+
+    const tracer = spawn('traceroute', [domain]);
+
+    tracer.stdout.on('data', (data) => {
+      trace.data += data.toString();
+    });
+
+    tracer.stderr.on('data', (data) => {
+      trace.data += data.toString();
+    });
+
+    tracer.on('error', (error) => {
+      trace.data += `\n\nERROR: ${error.message}\n\n`;
+    });
+
+    tracer.on('close', (code) => {
+      trace.data += `\n\nDONE: ${code}`;
+      results.traceroute[domain].push(trace);
+      resolvePromise();
+    });
+
+    // If it takes longer than two minutes, it probably already
+    // got the required info or it got stuck. In both cases, bonk it.
+    setTimeout(() => tracer.kill(), 120000);
+  });
+};
+
+async function main() {
   await Promise.allSettled([
     resolveForNS('openDNS', ['208.67.222.222']),
     resolveForNS('cloudflare', ['1.1.1.1']),
-    resolveForNS('google', ['8.8.8.8', '8.8.4.4'])
+    resolveForNS('google', ['8.8.8.8', '8.8.4.4']),
+
+    preformTraceroute('vanherk.io'),
+    preformTraceroute('google.com')
   ]);
 
   writeFileSync(resolvePath(dirname(fileURLToPath(import.meta.url)), 'output/results.json'), JSON.stringify(results, null, 2));
-}, 1000);
+
+  await new Promise((resolve) => setTimeout(resolve, 50000));
+
+  await main();
+};
+
+// Initialize
+await main();
